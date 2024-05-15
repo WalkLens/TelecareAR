4cb9195202d7bb8f9c704ee8f6061d6310535d22
kgy6022 <kgy6022@naver.com>
2024-05-15 19:21:41 +0900

    delete build files

--- _Build_1.0.0/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,4 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\snuh_cmti\Documents\GitHub\TelecareAR\_Build_1.0.0\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\snuh_cmti\Documents\GitHub\TelecareAR\_Build_1.0.0\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "C:\Users\snuh_cmti\Documents\GitHub\TelecareAR\_Build_1.0.0\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
--- _Build_1/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\User\Documents\GitHub\TelecareAR\_Build_1\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-api-debugger.cpp"
